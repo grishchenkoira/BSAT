52eceb89a456dcf8e7424cbdc8a7b944d24441ec
Irina Grishchenko <grishch.irina@gmail.com>
2023-10-17 21:13:52 +0300

    Add corrections to the description of the functions into Bio_Files_Processor.py

--- Bio_Files_Processor.py
+++ Bio_Files_Processor.py
@@ -5,10 +5,11 @@ def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str = Non
     """
     Function conver multiline fasta file into fasta with name line marked by '>' and the other line with sequence.
     Results save into new folder "Converted_data". If the folder already exists, new data is written to it.
+    
     :param input_fasta: Path to fasta-file with your seqs with. 
-    It is necessary to indicate the name along with extensions (.fasta)
+     !! It is necessary to indicate the name along with extensions (.fasta) !!
     :type input_fasta: str
-    :param output_fasta: Name of new fasta-file with your seqs in one line, default value is None
+    :param output_fasta: Name of new fasta-file with your seqs in one line, default value = None
     It is necessary to indicate the name along with extensions (.fasta)
     :type output_fasta: str
     :rtype: str
