16fa99a2f31b56561be7b5acc6a9cf3f8c48920a
Irina Grishchenko <grishch.irina@gmail.com>
2023-10-17 21:10:07 +0300

    Add function 'select_genes_from_gbk_to_fasta' into Bio_Files_Processor.py

--- Bio_Files_Processor.py
+++ Bio_Files_Processor.py
@@ -40,3 +40,73 @@ def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str = Non
                         new_seq_fasta.write(line.strip('\n'))
                     continue
         return 'All sequences processed!'
+
+
+def select_genes_from_gbk_to_fasta(input_gbk: str, genes: List[str], n_before: int = 1, n_after: int = 1, 
+                                   output_fasta: str = None) -> str:
+    '''
+    Function help to search neighbours of GOI (gene of interest). Function writes neighbours of GOI in new FASTA-file 
+    as: name of gene, protein sequence. Results save into new folder "Analyzed_data". If the folder already exists, 
+    new data is written to it.
+    
+    :input_gbk: Path to gbk-file with your seqs with.
+    !! It is necessary to indicate the name along with extensions (.gbk) !!
+    :type input_gbk: str
+    :param genes: Gene of interest names
+    :type genes: str
+    :param n_before: number of genes before GOI (>0), default value = 1
+    :type n_before: int
+    :param n_after: number of genes after GOI (>0), default value = 1
+    :type n_after: int
+    :output_fasta: Name of FASTA-file with neighbours of GOI (names and seqs), default value = None
+    :type output_fasta: str
+    :rtype: str
+    :return: Script completion message
+    
+    '''
+    if os.path.exists(os.path.join('.', 'Analyzed_data')) == False:
+        os.mkdir(os.path.join('.', 'Analyzed_data'))
+    genes_for_search = genes
+    genes_gbk = []
+    genes_for_search_in_gbk = []
+    neighbour_genes = dict()
+    if output_fasta == None:
+        output_fasta = 'output_for_gbk.fasta'
+    with open (input_gbk) as gbk:
+        for line in gbk:
+            if '/gene' in line:
+                genes_gbk += [line.strip().split('=')[1]]
+    for el in genes_for_search:
+        genes_for_search_in_gbk += [gn for gn in genes_gbk if el in gn]
+    for gene in genes_for_search_in_gbk:
+        gene_index = genes_gbk.index(gene)
+        if gene_index >= 0 and gene_index < (len(genes_gbk) - 1):
+            for i in range(1, n_before + 1):
+                neighbour_genes[(genes_gbk[gene_index - i])] = 0
+            for i in range(1, n_after + 1):
+                neighbour_genes[(genes_gbk[gene_index + i])] = 0
+        else:
+            for i in range(1, n_before + 1):
+                neighbour_genes[(genes_gbk[gene_index - i])] = 0
+            for i in range(1, n_after):
+                neighbour_genes[(genes_gbk[0 + i])] = 0
+    with open (input_gbk) as gbk:
+        counter = 0
+        gene_name = ''
+        for line in gbk:
+            for key in neighbour_genes:
+                if key in line:
+                    counter = 1
+                    gene_name = key
+                    continue
+            if counter != 0 and '/translation' in line:
+                counter = 0
+                neighbour_genes[gene_name] = line.strip().split('=')[1]
+                continue
+    with open (os.path.join('.', 'Analyzed_data', output_fasta), mode = 'w') as fasta:
+        for name, seq in neighbour_genes.items():
+            name = '>'+name.replace('\"','')
+            fasta.write(name + '\n')
+            fasta.write(seq.replace('\"','') + '\n')
+    return 'All sequences processed!'
+
